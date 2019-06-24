20124079ff55df8132c8cb40c08b019a65f63b9d
LINUX-PC <jesusrope98@outlook.com>
2019-06-24 20:41:06 +0200

    Updated example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,9 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Row collection',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: HomePage(),
     );
   }
@@ -20,103 +18,89 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Row collection'),
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(16),
-        children: <Widget>[
+      appBar: AppBar(title: Text('Row collection')),
+      body: ListView(children: <Widget>[
+        RowLayout.cardList(cards: <Widget>[
           Card(
-            child: RowLayout(
-              children: <Widget>[
-                RowItem.text('English', 'Hello'),
-                RowItem.text('Español', 'Hola'),
-                Separator.divider(),
-                RowItem.text('English', 'Bye'),
-                RowItem.text('Español', 'Adiós'),
-              ],
-            ),
+            child: RowLayout.body(children: <Widget>[
+              RowItem.text('English', 'Hello'),
+              RowItem.text('Español', 'Hola'),
+              Separator.divider(),
+              RowItem.text('English', 'Bye'),
+              RowItem.text('Español', 'Adiós'),
+            ]),
           ),
-          Separator.cardSpacer(),
           Card(
-            child: RowLayout(
-              children: <Widget>[
-                RowItem.text(
-                  'English',
-                  'Hello',
-                  titleStyle: TextStyle(color: Colors.blue),
-                  descriptionStyle: TextStyle(fontSize: 18),
-                ),
-                RowItem.text(
-                  'Español',
-                  'Hola',
-                  titleStyle: TextStyle(color: Colors.red),
-                  descriptionStyle: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Separator.divider(),
-                RowItem.text(
-                  'English',
-                  'Bye',
-                  titleStyle: TextStyle(color: Colors.blue),
-                  descriptionStyle: TextStyle(fontStyle: FontStyle.italic),
-                ),
-                RowItem.text(
-                  'Español',
-                  'Adiós',
-                  titleStyle: TextStyle(color: Colors.red),
-                  descriptionStyle: TextStyle(letterSpacing: 3),
-                ),
-              ],
-            ),
+            child: RowLayout.body(children: <Widget>[
+              RowItem.text(
+                'English',
+                'Hello',
+                titleStyle: TextStyle(color: Colors.blue),
+                descriptionStyle: TextStyle(fontSize: 18),
+              ),
+              RowItem.text(
+                'Español',
+                'Hola',
+                titleStyle: TextStyle(color: Colors.red),
+                descriptionStyle: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Separator.divider(),
+              RowItem.text(
+                'English',
+                'Bye',
+                titleStyle: TextStyle(color: Colors.blue),
+                descriptionStyle: TextStyle(fontStyle: FontStyle.italic),
+              ),
+              RowItem.text(
+                'Español',
+                'Adiós',
+                titleStyle: TextStyle(color: Colors.red),
+                descriptionStyle: TextStyle(letterSpacing: 3),
+              ),
+            ]),
           ),
-          Separator.cardSpacer(),
           Card(
-            child: RowLayout(
-              children: <Widget>[
-                RowItem.icon('Firefox', true),
-                RowItem.icon('Chrome', false),
-                Separator.divider(),
-                RowItem.icon(
-                  'Firefox',
-                  true,
-                  iconColor: Theme.of(context).accentColor,
-                ),
-                RowItem.icon(
-                  'Chrome',
-                  false,
-                  iconColor: Theme.of(context).accentColor,
-                ),
-              ],
-            ),
+            child: RowLayout.body(children: <Widget>[
+              RowItem.icon('Firefox', true),
+              RowItem.icon('Chrome', false),
+              Separator.divider(),
+              RowItem.icon(
+                'Firefox',
+                true,
+                iconColor: Theme.of(context).accentColor,
+              ),
+              RowItem.icon(
+                'Chrome',
+                false,
+                iconColor: Theme.of(context).accentColor,
+              ),
+            ]),
           ),
-          Separator.cardSpacer(),
           Card(
-            child: RowLayout(
-              children: <Widget>[
-                RowItem.clickable(
-                  'Android',
-                  'Click here',
-                  onTap: () => print('Clicked!'),
-                ),
-                RowItem.clickable(
-                  'iOS',
-                  'Click here',
-                  onTap: () => print('Clicked!'),
-                ),
-                Separator.divider(),
-                RowItem.clickable(
-                  'Windows Phone',
-                  'Don\'t click',
-                ),
-                RowItem.clickable(
-                  'Symbian',
-                  'Don\'t click',
-                ),
-              ],
-            ),
+            child: RowLayout.body(children: <Widget>[
+              RowItem.clickable(
+                'Android',
+                'Click here',
+                onTap: () => print('Clicked!'),
+              ),
+              RowItem.clickable(
+                'iOS',
+                'Click here',
+                onTap: () => print('Clicked!'),
+              ),
+              Separator.divider(),
+              RowItem.clickable(
+                'Windows Phone',
+                'Don\'t click',
+              ),
+              RowItem.clickable(
+                'Symbian',
+                'Don\'t click',
+              ),
+            ]),
           ),
-        ],
-      ),
+        ]),
+      ]),
     );
   }
 }
